e46d2fae3318e5669df30c9a7bf2ce041dfe2c85
unknown <uso>
2020-08-08 21:19:58 +0800

    update

--- src/container/admin/charts/Charts.jsx
+++ src/container/admin/charts/Charts.jsx
@@ -10,6 +10,8 @@ class Charts extends Component {
     category: [],
     isDisabled: false,
     isLoading: true,
+    sales: [],
+    inventorys: [],
   }
   componentDidMount() {
     this.getCategoryList()
@@ -19,33 +21,112 @@ class Charts extends Component {
     const data = categoryList.length ? categoryList : await reqCategoryList()
     if (data) {
       const category = []
-      data.forEach((item) => category.push(item.name))
-      this.setState({ category, isDisabled: false, isLoading: false })
+      const sales = []
+      const inventorys = []
+      data.forEach((item) => {
+        category.push(item.name)
+        sales.push(Math.floor(Math.random() * 1000))
+        inventorys.push(Math.floor(Math.random() * 1000))
+      })
+      this.setState({
+        category,
+        sales,
+        inventorys,
+        isLoading: false,
+        isDisabled: false,
+      })
     }
   }
-  getOption = () => ({
-    title: {
-      text: '',
-    },
-    tooltip: {},
-    xAxis: {
-      data: this.state.category,
-    },
-    yAxis: {},
-    series: [
-      {
-        name: '销量',
-        type: this.props.match.params.type,
-        data: [5, 20, 36, 10, 10, 20],
-      },
-    ],
-  })
+  getOption = (type) => {
+    const { category, sales, inventorys } = this.state
+    const salesData = []
+    const inventorysData = []
+    let option
+    category.forEach((item, i) => {
+      salesData.push({ value: sales[i], name: item })
+      inventorysData.push({ value: inventorys[i], name: item })
+    })
+    if (type === 'pie') {
+      option = {
+        title: {
+          text: '',
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a}<br/>{b}:{c}({d}%)',
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: category,
+        },
+        series: [
+          {
+            name: '销量',
+            type,
+            radius: '50%',
+            center: ['30%', '50%'],
+            data: salesData,
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0,0,0,.2)',
+              },
+            },
+          },
+          {
+            name: '库存',
+            type,
+            radius: '50%',
+            center: ['60%', '50%'],
+            data: inventorysData,
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0,0,0,.2)',
+              },
+            },
+          },
+        ],
+      }
+    } else {
+      option = {
+        title: {
+          text: '',
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量', '库存'],
+        },
+        xAxis: {
+          data: category,
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type,
+            data: sales,
+          },
+          {
+            name: '库存',
+            type,
+            data: inventorys,
+          },
+        ],
+      }
+    }
+    return option
+  }
   update = () => {
     this.setState({ isDisabled: true })
     this.getCategoryList()
   }
   render() {
     const { isDisabled, isLoading } = this.state
+    const { type } = this.props.match.params
     return (
       <Card
         loading={isLoading}
@@ -59,7 +140,7 @@ class Charts extends Component {
           </Button>
         }
       >
-        <ReactEcharts option={this.getOption()} />
+        <ReactEcharts option={this.getOption(type)} />
       </Card>
     )
   }
